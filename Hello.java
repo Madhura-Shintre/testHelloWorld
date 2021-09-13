409a0b17b9a3346e2fec3604ce3107492f59bf64
Madhura-Shintre <77835219+Madhura-Shintre@users.noreply.github.com>
2021-09-13 13:33:27 +0530

    Branch1 (#2)
    * Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main( String []args ) {
-        System.out.println( "Hello World...branch 1" );
+        System.out.println( "Hello my World...branch 1 _ squash" );
     }
 }
